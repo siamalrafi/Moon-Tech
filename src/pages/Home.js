fed1c6e64671aecf231d03102b69598fb33b15cf
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-01 17:52:03 +0600

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import ProductCard from '../components/ProductCard';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+    fetch('http://localhost:5000/products')
+      .then(res => res.json())
+      .then(data => setProducts(data.data))
+  }, []);
+
+  console.log(products);
+
+
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
+
+
+      {
+        products?.map(product =>
+          <ProductCard key={product._id}  product={product}>
+
+          </ProductCard >
+        )
+      }
+
     </div>
   );
 };
